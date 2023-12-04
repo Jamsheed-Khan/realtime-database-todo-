2355520a88434f79481e6a16e53537fcc983db06
Jamsheed-Khan <jamshedkh365@gmail.com>
2023-12-04 18:19:37 +0500

    Almost done

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-let getinp = document.querySelector("#inp");
-let getaddbtn = document.querySelector("#add") 
+let getaddbtn = document.querySelector("#add")
 let getul = document.querySelector("#ul");
 let getclearbtn = document.querySelector("#clear");
 
@@ -7,7 +6,7 @@ let getclearbtn = document.querySelector("#clear");
 
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getDatabase,ref, onValue, set  } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+import { getDatabase, ref, onValue, set, push, remove, update } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 const firebaseConfig = {
   apiKey: "AIzaSyDh94-doq2N1iunKqOWNj-MEZVuhV68sII",
   authDomain: "realtime-database-todo-app.firebaseapp.com",
@@ -21,102 +20,77 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
+const userId = localStorage.getItem("userid")
+
+
+getaddbtn.addEventListener('click', () => {
+  let getinp = document.querySelector("#inp");
+
+  console.log(userId);
+
+  set(push(ref(db, `users/${userId}/todos/`)), {
+    name: getinp.value,
+  });
+
+  getinp.value = "";
+})
 
 
 
-getaddbtn.addEventListener('click',()=>{
-  function writeUserData(userId, name,) {
-  
-    set(ref(db, 'users/',userId,name), {
-      userId: Math.random()*1000,  
-      name:getinp.value,
-    });
+const getTodos = () => {
+
+  const todoRef = ref(db, `users/${userId}/todos`);
+  onValue(todoRef, (snapshot) => {
+    getul.innerHTML = "";
+    const data = snapshot.val();
+    if (data) {
+      getclearbtn.style.display = "block";
+      for (const key in data) {
+        const todo = data[key].name;
+        getul.innerHTML +=
+          `<li class="list">
+    <div class="liTextDiv"><p>${todo}</p></div>
+    <div class="divClass">
+    <i onclick="editTodo(this,'${key}')"  class="fa fa-pencil-square edit"></i>
+    <i onclick="delTodo('${key}')"  class="fa fa-trash deleted"></i>
+    </div>
+    </li>`
+      }
+    } else {
+      getclearbtn.style.display = "none";
+    }
+
+  });
+
+}
+
+getTodos()
+
+
+async function delTodo(id) {
+  const delTodo = ref(db, `users/${userId}/todos/${id}`);
+  await remove(delTodo)
+}
+
+function editTodo(e, id) {
+  console.log(e.parentNode.parentNode.textContent);
+  let editToDo = prompt("Edit your ToDo", e.parentNode.parentNode.textContent)
+  const editTodoRef = ref(db, `users/${userId}/todos/${id}`);
+
+  const editTodo = {
+    name: editToDo
   }
-  getinp.value == ""
-  //  console.log()
-  writeUserData(getinp.value)
-  // getinp.value == ""
-    })
+
+  update(editTodoRef, editTodo)
+}
 
 
+getclearbtn.addEventListener("click", async () => {
+  const delTodo = ref(db, `users/${userId}/todos/`);
+  await remove(delTodo)
+})
 
-
-const starCountRef = ref(db, 'users/' );
-onValue(starCountRef, (snapshot) => {
-  const data = snapshot.val();
-getul.innerHTML +=
-`<li id="${data.userId}" class="list">
-<div class="liTextDiv"><p>${data.name}</p></div>
-<div class="divClass">
-<i onclick="editTodo(this)"  class="fa fa-pencil-square edit"></i>
-<i onclick="delTodo(this)"  class="fa fa-trash deleted"></i>
-</div>
-</li>`
-  console.log(data) 
-});
-
-
-
-
-
-
-
-
-
-// getaddbtn.addEventListener("click" , async ()=>{
-
-//     let getli = document.createElement("li");
-//     getli.className = "list"
-//     let liTextDiv = document.createElement("div");
-//     liTextDiv.setAttribute("class", "liTextDiv")
-//     let liTextPara = document.createElement("p");
-//     let litext = document.createTextNode(getinp.value);
-
-      
-//     liTextPara.appendChild(litext);
-//     liTextDiv.appendChild(liTextPara);
-//     getli.appendChild(liTextDiv)
-//     getul.appendChild(getli);
-//     getinp.value = ""
-    
-//     let div = document.createElement("div");
-//     div.classList.add("divClass");
-    
-//     let editIcon = document.createElement("i");
-//     editIcon.setAttribute('onclick' , 'editTodo(this)')
-//     editIcon.className = 'edit'
-//     editIcon.classList.add("fa");   
-//     editIcon.classList.add("fa-pencil-square");
-    
-    
-//     let delIcon = document.createElement("i");
-//     delIcon.setAttribute("onclick" , "delTodo(this)")
-//     delIcon.className = "deleted"
-//     delIcon.classList.add("fa");
-//     delIcon.classList.add("fa-trash");
-//     div.appendChild(editIcon)
-//     div.appendChild(delIcon)
-//     getli.appendChild(div);
-    
-    
-//   })
-  
-
-//   function delTodo(event){
-//     event.parentNode.parentNode.remove();
-//   }
-  
-//   function editTodo(e){
-//     let editToDo = prompt("Edit your ToDo" , e.parentNode.parentNode.firstChild.textContent)
-//     e.parentNode.parentNode.firstChild.textContent = editToDo
-//   }
-
-
-//   getclearbtn.addEventListener("click" , ()=>{
-//     getul.innerHTML = ""
-//   })
-  
-//   window.delTodo = delTodo
-//   window.editTodo = editTodo
+window.delTodo = delTodo
+window.editTodo = editTodo
 
 
